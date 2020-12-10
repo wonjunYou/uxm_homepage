9e641fc6636548e4d31b7ac2cf251e9d3e11103c
Sol Choi <habrush3024@gmail.com>
2020-12-10 23:42:12 +0900

    notice_write and notice_show add

--- routes/notice.js
+++ routes/notice.js
@@ -4,5 +4,13 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     res.render('notice',{title:"Notice"});
   });
+
+router.get('/show/:id', function(req, res, next) {
+  res.render('notice_show');
+});
+
+router.get('/write',function(req, res, next) {
+  res.render('notice_write');
+});
   
 module.exports = router;
