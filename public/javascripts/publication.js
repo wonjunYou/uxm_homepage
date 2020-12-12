684f43256b264d189e872598046169cea6cba07e
DOJIN <djyou128@gmail.com>
2020-12-12 23:29:00 +0900

    photo page js update

--- public/javascripts/publication.js
+++ public/javascripts/publication.js
@@ -1,12 +1,4 @@
 $(document).ready(function () {
-	$('.popup-gallery').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true,
-		},
-	});
 	$('.sub_menu > 	ul > li > a').click(function () {
 		$('.on').removeClass('on');
 		$(this).addClass('on');
