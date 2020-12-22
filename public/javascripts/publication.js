b2d48d409d77532684ad9558c5244e007ae30b6a
DOJIN <djyou128@gmail.com>
2020-12-22 09:34:41 +0900

    update publication tab effect

--- public/javascripts/publication.js
+++ public/javascripts/publication.js
@@ -21,25 +21,40 @@ $(document).ready(function () {
 	// });
 
 	$('[class^="pub_tr"]').css({ display: 'none' });
-	$('.pub_tr1').css({ display: '' });
-	$('.p_content tbody tr:not([style*="display:none"]):nth-child(2n+1)').css({
-		backgroundColor: 'rgba(128,128,128,0.2)',
-	});
+	$('.pub_tr1')
+		.css({ display: '' })
+		.each(function (index, item) {
+			if (index % 2 == 0) {
+				$(item).css('backgroundColor', 'rgba(128,128,128,0.2)');
+			}
+		});
 	$('.pub_sub_menu > 	ul > li').click(function () {
 		$('.on').removeClass('on');
 		$(this).addClass('on');
 		$('[class^="pub_tr"]').hide();
+		let class_name = '.pub_tr';
 		if ($(this).text() == '해외학술지') {
-			$('.pub_tr1').css({ display: '' });
+			class_name += '1';
+			// $('.pub_tr1').css({ display: '' });
 		}
 		if ($(this).text() == '해외학회발표') {
-			$('.pub_tr2').css({ display: '' });
+			class_name += '2';
+			// $('.pub_tr2').css({ display: '' });
 		}
 		if ($(this).text() == '국내학술지') {
-			$('.pub_tr3').css({ display: '' });
+			class_name += '3';
+			// $('.pub_tr3').css({ display: '' });
 		}
 		if ($(this).text() == '국내학회발표') {
-			$('.pub_tr4').css({ display: '' });
+			class_name += '4';
+			// $('.pub_tr4').css({ display: '' });
 		}
+		$(class_name)
+			.css({ display: '' })
+			.each(function (index, item) {
+				if (index % 2 == 0) {
+					$(item).css('backgroundColor', 'rgba(128,128,128,0.2)');
+				}
+			});
 	});
 });
