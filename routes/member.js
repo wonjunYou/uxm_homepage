f123f17f661fbd8813f94def2ecc82c63604c84d
DOJIN <djyou128@gmail.com>
2020-12-24 23:54:26 +0900

    update member.pug from db

--- routes/member.js
+++ routes/member.js
@@ -10,48 +10,53 @@ var storage = multer.diskStorage({
 		cb(null, './public/images/');
 	},
 	filename: function (req, file, cb) {
-		cb(
-			null, `${req.body.name_en}`
-		);
+		cb(null, `${req.body.name_en}.jpg`);
 	},
 });
-var upload = multer({storage : storage});
+var upload = multer({ storage: storage });
 
-router.get('/', function(req, res, next) {
-  //불러오는것 짜야함
-  res.render('member', { title: 'Express' });
+router.get('/', function (req, res, next) {
+	Member.find(function (err, mem) {
+		mem.sort(function (a, b) {
+			if (a.rank > b.rank) {
+				return 1;
+			}
+			return -1;
+		});
+		res.render('member', { members: mem });
+	});
+	// //불러오는것 짜야함
+	// res.render('member', { title: 'Express' });
 });
 
-router.get('/upload',function (req, res, next){
-  res.render('member_upload');
-})
+router.get('/upload', function (req, res, next) {
+	res.render('member_upload');
+});
 
 //upload
-router.post('/member_up',upload.single('input_img'),function(req,res){
-  var members = new Member();
-  console.log(__dirname);
-  var memberObj = {
-    rank : req.body.selected_rank,
-    name_en : req.body.input_name_en,
-    name_kr : req.body.input_name_kr,
-    affiliation : req.body.input_affiliation,
-    main_research : req.body.input_main_research,
-    img: {
-      // 여기 path가 잘못 된 것 같은데,,
-      data: fs.readFileSync(path.join(__dirname+'/../public/images/')),
-      contentType : 'image/*'
-    }
- }
- members.create(memberObj , function(err, item) {
-   if(err){
-     console.log(err);
-   }
-    else{
-      res.redirect('/member');
-   }
- });
+router.post('/member_up', upload.single('input_img'), function (req, res) {
+	var members = new Member();
+	console.log(__dirname);
+	var memberObj = {
+		rank: req.body.selected_rank,
+		name_en: req.body.input_name_en,
+		name_kr: req.body.input_name_kr,
+		affiliation: req.body.input_affiliation,
+		main_research: req.body.input_main_research,
+		img: {
+			// 여기 path가 잘못 된 것 같은데,,
+			data: fs.readFileSync(path.join(__dirname + '/../public/images/')),
+			contentType: 'image/*',
+		},
+	};
+	members.create(memberObj, function (err, item) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.redirect('/member');
+		}
+	});
 });
 //수정 & 삭제
-
 
 module.exports = router;
