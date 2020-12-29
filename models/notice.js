fa0608596c7131d888362b4cb5d784ba4fe98bfc
Sol Choi <habrush3024@gmail.com>
2020-12-29 17:55:05 +0900

    notice update

--- models/notice.js
+++ models/notice.js
@@ -12,7 +12,6 @@ const noticeSchema = new mongoose.Schema(
 		views: { type: Number, default: 0 },
 		date: { type: Date, default: Date.now() },
 		numId: {type:Number},
-		// attachment: String,
 		createdAt: { type: Date, default: Date.now },
 	},
 	{ collection: 'notice' }
